8eb4c0c6d76d59b5f1d29853b1ba995cf76ba6e8
Jamie Svanberg <jamie_svanberg@hotmail.com>
2021-09-20 14:31:42 +0200

    HighscoreViewModel är borttagen och ersatt

--- UnusArmatusLattro/UnusArmatusLattro/ViewModels/TopListViewModel.cs
+++ UnusArmatusLattro/UnusArmatusLattro/ViewModels/TopListViewModel.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using UnusArmatusLattro.Models;
 using UnusArmatusLattro.Repositories;
 
 namespace UnusArmatusLattro.ViewModels
 {
-    public class HighscoreViewModel : BaseViewModel
+
+    public class TopListViewModel : BaseViewModel
     {
-      
         UserRepository db = new UserRepository();
-
-        public HighscoreViewModel()
-        {
-          
-        }
-
-        
     }
 }
