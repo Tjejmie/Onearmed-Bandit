934a24c0eb2c25a825dda38d59f86e75c10a4c45
jnordqvist <73642406+jnordqvist@users.noreply.github.com>
2021-09-22 11:04:59 +0200

    skapat popup med instruktioner i spinspel

--- UnusArmatusLattro/UnusArmatusLattro/Data/Difficulties.cs
+++ UnusArmatusLattro/UnusArmatusLattro/Data/Difficulties.cs
@@ -8,6 +8,6 @@ namespace UnusArmatusLattro.Data
 {
     public enum Difficulties
     {
-        Easy = 1000, Normal = 500, Hard = 750, Betting = 510
+        Easy = 1000, Normal = 500, Hard = 20, Betting = 510
     }
 }
