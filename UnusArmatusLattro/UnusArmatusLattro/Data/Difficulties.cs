425ec83dda5737b6d110833c4ec545d281a199f5
Jamie Svanberg <jamie_svanberg@hotmail.com>
2021-09-22 11:08:45 +0200

    Ändringar i gränssnitt och hastighet

--- UnusArmatusLattro/UnusArmatusLattro/Data/Difficulties.cs
+++ UnusArmatusLattro/UnusArmatusLattro/Data/Difficulties.cs
@@ -8,6 +8,6 @@ namespace UnusArmatusLattro.Data
 {
     public enum Difficulties
     {
-        Easy = 1000, Normal = 500, Hard = 750, Betting = 510
+        Easy = 700, Normal = 450, Hard = 150, Betting = 300
     }
 }
